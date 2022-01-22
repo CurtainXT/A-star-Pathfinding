9bc13948705b60f3e226019a26534a18a28845cd
CurtainXT <47293367+CurtainXT@users.noreply.github.com>
2022-01-22 14:20:31 +0800

    Implement a simple A* algorithm

---
+++ Grid.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Grid的GameObject X、Y轴要在场景正中
+public class Grid : MonoBehaviour
+{
+    // 用来存储unwalkable的layermask
+    public LayerMask unwalkableMask;
+    // grid的大小
+    public Vector2 gridWorldSize; // Vector2的y对应世界坐标中的z轴
+    // grid中的node的半径（node立方体边长的一半）
+    public float nodeRadius;
+    // 玩家的位置
+    public Transform player;
+
+    // grid是二位的node数组
+    Node[,] grid;
+    // grid中的node的直径
+    float nodeDiameter;
+    // Grid中的node数量
+    int gridSizeX, gridSizeY;
+
+    // 路径
+    public List<Node> path;
+
+    private void Start()
+    {
+        // 根据grid的尺寸和node的尺寸计算node的数量并填入二维数组
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    // 创建grid实例
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        // 计算得到grid（从上往下看）左下角的世界坐标位置
+        Vector3 worldButtomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2; //forward没错，y对应node的z坐标
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                // 计算每一个node的世界坐标位置
+                Vector3 worldPoint = worldButtomLeft +
+                    Vector3.right * (x * nodeDiameter + nodeRadius) +
+                    Vector3.forward * (y * nodeDiameter + nodeRadius);
+
+                // 判断是否有obstacles，如果有就将node设置为unwalkable
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                // 创建每个node实例并给成员赋值
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
+            }
+        }
+    }
+
+    // 获取节点的相邻节点
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= -1; x++)
+        {
+            for (int y = 0; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    // 这就是节点自己
+                    continue;
+                }
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                // 考虑Node位于Grid边界的情况
+                if(checkX >= 0 && checkY < gridSizeY && checkX < gridSizeX && checkY >= 0)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    // 通过世界坐标获得Node
+    public Node GetNodeFromWorldPoint(Vector3 worldPosition)
+    {
+        // 通过将坐标换算为Grid中的百分比位置来获取Node
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y; //grid的y长对应世界坐标系的z
+
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX); //减一是因为gridSize是1开始，我们需要index
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
+
+    // 在Scene面板中显示grid
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if (grid != null)
+        {
+            Node playerNode = GetNodeFromWorldPoint(player.position);
+            foreach(Node node in grid)
+            {
+                Gizmos.color = node.walkable ? Color.white : Color.red;
+                if(playerNode == node)
+                {
+                    Gizmos.color = Color.cyan;
+                }
+                if(path != null)
+                {
+                    if (path.Contains(node))
+                        Gizmos.color = Color.green;
+                }
+                Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+}
---
+++ Pathfinding.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+
+    public Grid grid;
+
+    private void Awake()
+    {
+        grid = this.GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        // 获取起点终点
+        Node startNode = grid.GetNodeFromWorldPoint(startPos);
+        Node targetNode = grid.GetNodeFromWorldPoint(targetPos);
+        
+        // Open列表 存放所有预选的节点
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closeSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                // 寻找一个比当前节点更优的节点 晚点再来做优化
+                if(openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].gCost < currentNode.gCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closeSet.Add(currentNode);
+            
+            // 碰到终点了
+            if (currentNode == targetNode)
+            {
+                // 回溯节点以获取路径
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            // 查看每个相邻节点
+            foreach (Node neighbourNode in grid.GetNeighbours(currentNode))
+            {
+                // 如果相邻节点unwalkable或者已经在closeSet里面了 啥也不干
+                if(!neighbourNode.walkable || closeSet.Contains(neighbourNode))
+                {
+                    continue;
+                }
+                // 计算从当前节点来看的neighbourNode的gCost
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbourNode);
+                // 如果新的gCost更小 或者这是第一次考虑此neighbourNode
+                if(newMovementCostToNeighbour < neighbourNode.gCost || !openSet.Contains(neighbourNode))
+                {
+                    // 更新此neighbourNode的Cost
+                    neighbourNode.gCost = newMovementCostToNeighbour;
+                    neighbourNode.hCost = GetDistance(neighbourNode, targetNode);
+                    neighbourNode.parent = currentNode;
+
+                    if(!openSet.Contains(neighbourNode))
+                    {
+                        openSet.Add(neighbourNode);
+                    }
+                }
+            }
+        }
+    }
+
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while(currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if(dstX > dstY)
+        {
+            return 14 * dstY + 10 * (dstX - dstY);
+        }
+        else
+        {
+            return 14 * dstX + 10 * (dstY - dstX);
+        }
+    }
+}
